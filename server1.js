f4d7b14b78a3d938564aacc57236997770c845e9
prem kumar <premkumar.kambala@gmail.com>
2023-06-11 05:03:12 +0530

    Revert "commit1"
    This reverts commit a5d0f820636cb0f4f2824b1fc2eb1eab4f1ff5d6.

--- server1.js
+++
@@ -1,9 +0,0 @@
-const express=require('express') 
-const http=require('http')
-
-const server=http.createServer((req,res)=>{
-    console.log("something...")
-    res.end("home")
-})
-
-server.listen(5000)
